edfa9b4e161fc310405f9f75060bd0cea8e51055
Gregory Hawman <g.hawman@gmail.com>
2022-03-01 19:36:58 -0500

    added a media query

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Typescript Todo</span>
+        <span className="heading">Typescript-Todo</span>
         <InputField 
           todo={todo}
           setTodo={setTodo} 
